638f63641413bb0e8e2e47a5a5587f2d8a0761c7
platinum-complex <platinumnotsimple@gmail.com>
2023-08-17 10:10:33 -0400

    added descriptions

---
+++ Basic Data Types Activity/story_time.js
@@ -0,0 +1,36 @@
+// TODO: Create 4-String variables to introduce develop your story.
+// YOUR CODE GOES HERE
+let power = "immortal"
+let name = "Flare";
+let title = "Champion of Ash Dragons";
+let factionName = "Ice-Ash Alliance";
+
+// TODO: Create 3-String variables to set the time period of your story or discuss other number elements.
+// YOUR CODE GOES HERE
+let age = 2000;
+let factions = 4;
+let timeLeft = 9;
+
+// TODO: Create 1-Array variable to show a collection of items your character might have.
+// YOUR CODE GOES HERE
+let weaponsUsed = ['Sword', 'Wand', 'Ninja Stars', 'Dragon Fruit'];
+
+// TODO: Create 1-Boolean variable to demonstrate a true or false scenario.
+// YOUR CODE GOES HERE
+let hasFavWeapon = false;
+
+// TODO: Print your story to the console.
+// YOUR CODE GOES HERE
+console.log("Ah, hello! Welcome to my kingdom. My name is " + name + ".");
+console.log("I'm the " + title + ".");
+console.log("As one of the two champions of the " + factionName + ", I use many different weapons.");
+console.log("The dragons are divided by elements, each with their own special abilities. For example, we Ash dragons are " + power + ".");
+console.log("We can be subdued, to be sure, to sleep for as long as centuries, but our wounds always heal and we always awaken again. No other elements can say the same!")
+console.log("Because of our powers, I'm " + age + " years old. I am one of the most ancient dragons there is, and all those older than myself are too Ash dragons.");
+console.log("Before my time, there was a great civil war, and the once unified elements split into " + factions + " factions that war to this day. We have allied with Ice temporarily. For now, you'll be safe to explore.");
+console.log("You can stay for as long as " + timeLeft + " days, but if by then you are still here, you will become a dragon like us and will be forced to join us in the war." + "\n")
+console.log("Before you go off to adventure, does anyone have any questions?");
+console.log("Me: Do you have a favorite weapon?");
+console.log("Dragon: " + hasFavWeapon + "; Ice's terms for our alliance include that dragons relenquish the right to all opinions, favorites included. For the moment, they dominate the alliance, and their word is law.");
+console.log("But it is within our rules to say objectively that my most effective weapon is my " + weaponsUsed[3] + "." + "\n");
+console.log("You are free to explore my world for as long as " + timeLeft + " days and return to Earth. But stay any longer and you shall not leave 'till peace and power reside in the thrones of our kingdom!");
--- Fortune Teller Activity/README.md
+++ Fortune Teller Activity/README.md
@@ -1,1 +1,2 @@
 # Fortune Teller Activity
+Generates a fortune based on a random number from 1-10.
